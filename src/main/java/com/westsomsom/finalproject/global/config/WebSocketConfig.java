b021483ecb79506768fcf63f9015b1c5f2197488
karam Choi <riverchoi2523@gmail.com>
2024-12-20 23:55:57 +0900

    FEAT:예약 기능 구현

--- src/main/java/com/westsomsom/finalproject/global/config/WebSocketConfig.java
+++ src/main/java/com/westsomsom/finalproject/global/config/WebSocketConfig.java
@@ -2,24 +2,35 @@ package com.westsomsom.finalproject.global.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocket
 @RequiredArgsConstructor
-public class WebSocketConfig implements WebSocketConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final WebSocketHandler webSocketHandler;
 
-    @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry
                 // /ws/conn 경로로 WebSocket 연결을 허용
                 .addHandler(webSocketHandler, "/ws/conn")
                 // CORS 허용
                 .setAllowedOrigins("*");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/queue", "/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-reservation")
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 }
