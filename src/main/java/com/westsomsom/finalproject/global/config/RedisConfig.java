0e9d1f8fb10bb96cea052ee8239324ec1ec427b4
karam Choi <riverchoi2523@gmail.com>
2024-12-26 20:35:15 +0900

    ADD:예약 가능 시간 확인 로직 추가

--- src/main/java/com/westsomsom/finalproject/global/config/RedisConfig.java
+++ src/main/java/com/westsomsom/finalproject/global/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.westsomsom.finalproject.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,9 +11,14 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String ELASTICACHE_ENDPOINT; // ElastiCache 엔드포인트 입력
+    @Value("${spring.data.redis.port}")
+    private int ELASTICACHE_PORT; // 기본 Redis 포트
+
     @Bean // Redis 설정
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379);
+        return new LettuceConnectionFactory(ELASTICACHE_ENDPOINT, ELASTICACHE_PORT);
     }
 
     @Bean
