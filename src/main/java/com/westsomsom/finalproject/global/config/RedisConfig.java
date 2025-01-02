7174360a82ed3feb4b783204088bb548ae33c26e
karam Choi <riverchoi2523@gmail.com>
2025-01-02 23:14:16 +0900

    FIX:팝업스토어 redis 적용

--- src/main/java/com/westsomsom/finalproject/global/config/RedisConfig.java
+++ src/main/java/com/westsomsom/finalproject/global/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.westsomsom.finalproject.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -16,12 +17,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
     @Value("${spring.data.redis.host}")
     private String ELASTICACHE_ENDPOINT; // ElastiCache 엔드포인트 입력
     @Value("${spring.data.redis.port}")
     private int ELASTICACHE_PORT; // 기본 Redis 포트
 
-    @Bean // TLS 활성화된 Redis 설정
+    @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         // RedisStandaloneConfiguration에 호스트와 포트 설정
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
@@ -42,14 +44,16 @@ public class RedisConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
+        // ObjectMapper 설정
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.registerModule(new JavaTimeModule()); // LocalDateTime, LocalDate 처리
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // ISO 8601 형식 사용
 
+        // Redis 직렬화 설정
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
-        template.setHashKeySerializer(new StringRedisSerializer()); // 해시의 키도 문자열로 저장
+        template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
         return template;
     }
 }
-
