17f0094f43b6a7a0fc654c32abb2f8d602d0b8d9
seplease <seplease@naver.com>
2025-01-03 17:31:28 +0900

    fix: 팝업 스토어 정보 조회 redis 적용 전 버전

--- src/main/java/com/westsomsom/finalproject/store/api/StoreController.java
+++ src/main/java/com/westsomsom/finalproject/store/api/StoreController.java
@@ -1,105 +1,35 @@
 package com.westsomsom.finalproject.store.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.westsomsom.finalproject.store.application.StoreService;
 import com.westsomsom.finalproject.store.domain.Store;
 import com.westsomsom.finalproject.store.dto.SearchRequestDto;
 import com.westsomsom.finalproject.store.dto.SearchResponseDto;
-import com.westsomsom.finalproject.store.dto.StoreRequestDto;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Optional;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class StoreController {
 
-    private final RedisTemplate<String, Object> redisTemplate;
     private final StoreService storeService;
-    private final ObjectMapper objectMapper; // ObjectMapper 추가
-    @Value("${spring.data.redis.cache.store}")
-    private String cachePrefix; // Redis cache prefix for stores
 
-    @PostMapping("/api/store")
-    public ResponseEntity<Store> createStore(@RequestBody StoreRequestDto storeRequestDto) {
-        Store store = storeService.createStore(storeRequestDto);
-        return ResponseEntity.ok(store);
-    }
-
-//    @GetMapping("/api/home")
-//    public ResponseEntity<Page<Store>> getAllStores(
-//            @RequestParam(value = "page", defaultValue = "0") Integer page
-//    ) {
-//        Page<Store> stores = storeService.getAllStores(PageRequest.of(page, 20));
-//        return ResponseEntity.ok(stores);
-//    }
-
-    //@Cacheable(value = "stores", key = "#page")
     @GetMapping("/api/home")
-    public ResponseEntity<List<Store>> getAllStores(
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "size", defaultValue = "20") Integer size
+    public ResponseEntity<Page<Store>> getAllStores(
+            @RequestParam(value = "page", defaultValue = "0") Integer page
     ) {
-        List<Store> storeList = new ArrayList<>();
-
-        // Redis에서 전체 데이터를 조회
-        log.info("Redis에서 전체 데이터를 조회 중... cachePrefix: {}", cachePrefix);
-        Map<Object, Object> allStores = redisTemplate.opsForHash().entries(cachePrefix);
-
-        if (allStores.isEmpty()) {
-            log.info("Redis에 데이터가 없습니다. DB에서 데이터를 조회 중...");
-
-            // DB에서 데이터 조회
-            Page<Store> stores = storeService.getAllStores(PageRequest.of(page, size));
-            storeList = stores.getContent();
-
-            // Redis에 저장
-            log.info("Redis에 데이터 저장 중...");
-            stores.getContent().forEach(store -> {
-                redisTemplate.opsForHash().put(cachePrefix, String.valueOf(store.getStoreId()), store);
-            });
-        } else {
-            log.info("Redis에서 데이터를 가져와 페이징 처리 중...");
-
-            // Redis에서 데이터를 가져와 List로 변환
-            List<Store> allStoresList = allStores.values().stream()
-                    .map(value -> {
-                        try {
-                            // GenericJackson2JsonRedisSerializer로 직렬화된 데이터를 Store 객체로 변환
-                            String json = objectMapper.writeValueAsString(value);
-                            return objectMapper.readValue(json, Store.class);
-                        } catch (Exception e) {
-                            log.error("Redis 데이터 변환 중 오류 발생", e);
-                            return null;
-                        }
-                    })
-                    .filter(Objects::nonNull)
-                    .sorted(Comparator.comparing(Store::getStoreId)) // 정렬
-                    .collect(Collectors.toList());
-
-            // 페이징 처리
-            int start = page * size;
-            int end = Math.min(start + size, allStoresList.size());
-
-            if (start < allStoresList.size()) {
-                storeList = allStoresList.subList(start, end);
-                log.info("팝업 스토어 정보 전체 조회 완료");
-            } else {
-                log.warn("요청한 페이지 범위가 데이터 크기를 초과했습니다. 빈 리스트 반환.");
-            }
-        }
-
-        return ResponseEntity.ok(storeList);
+        Page<Store> stores = storeService.getAllStores(PageRequest.of(page, 20));
+        return ResponseEntity.ok(stores);
     }
 
     @GetMapping("/api/search")
--- src/main/java/com/westsomsom/finalproject/store/application/StoreService.java
+++ src/main/java/com/westsomsom/finalproject/store/application/StoreService.java
@@ -4,49 +4,19 @@ import com.westsomsom.finalproject.store.dao.StoreRepository;
 import com.westsomsom.finalproject.store.domain.Store;
 import com.westsomsom.finalproject.store.dto.SearchRequestDto;
 import com.westsomsom.finalproject.store.dto.SearchResponseDto;
-import com.westsomsom.finalproject.store.dto.StoreRequestDto;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class StoreService {
 
     private final StoreRepository storeRepository;
-    private final RedisTemplate<String, Object> redisTemplate;
-
-    @Value("${spring.data.redis.cache.store}")
-    private String cachePrefix; // Redis cache prefix for stores
-
-    public Store createStore(StoreRequestDto storeRequestDto) {
-        // Create a new store entity from the request DTO
-        Store store = Store.builder()
-                .storeName(storeRequestDto.getStoreName())
-                .storeBio(storeRequestDto.getStoreBio())
-                .startDate(storeRequestDto.getStartDate())
-                .finDate(storeRequestDto.getFinDate())
-                .storeCategory(storeRequestDto.getStoreCategory())
-                .storeLoc(storeRequestDto.getStoreLoc())
-                .reservationStart(storeRequestDto.getReservationStart())
-                .reservationFin(storeRequestDto.getReservationFin())
-                .build();
-        log.info(store.getStoreName());
-        Store savedStore = storeRepository.save(store);
-        log.info(savedStore.getStoreId()+"");
-        log.info("cachePrefix {}",cachePrefix);
-        redisTemplate.opsForHash().put(cachePrefix, String.valueOf(savedStore.getStoreId()), savedStore);
-
-        return savedStore;
-    }
 
     public Page<Store> getAllStores(Pageable pageable) {
         return storeRepository.getAllStores(pageable);
