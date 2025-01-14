615114cfb6148985b0c129bc892b547eb5fe9d91
seplease <seplease@naver.com>
2025-01-14 17:23:45 +0900

    Fix: ChatMessageDto - JsonCreator 생성

--- src/main/java/com/westsomsom/finalproject/chat/dto/ChatMessageDto.java
+++ src/main/java/com/westsomsom/finalproject/chat/dto/ChatMessageDto.java
@@ -1,12 +1,13 @@
 package com.westsomsom.finalproject.chat.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Builder
 @Getter
 @Setter
-@AllArgsConstructor
+//@AllArgsConstructor
 @NoArgsConstructor
 public class ChatMessageDto {
     // 메시지  타입 : 입장, 채팅, 퇴장
@@ -18,4 +19,15 @@ public class ChatMessageDto {
     private int storeId; // 방번호
     private String sender; // 메시지 보낸 사람
     private String message; // 메시지
+
+    @JsonCreator
+    public ChatMessageDto(@JsonProperty("messageType") MessageType messageType,
+                          @JsonProperty("storeId") int storeId,
+                          @JsonProperty("sender") String sender,
+                          @JsonProperty("message") String message) {
+        this.messageType = messageType;
+        this.storeId = storeId;
+        this.sender = sender;
+        this.message = message;
+    }
 }
