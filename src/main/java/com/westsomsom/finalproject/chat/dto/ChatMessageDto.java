04a32cc02aae1b8826c9df04bdae1e64211b1fa5
seplease <seplease@naver.com>
2025-01-14 17:42:09 +0900

    Fix: ChatMessageDto - JsonCreator 제거, 기본 생성자 추가

--- src/main/java/com/westsomsom/finalproject/chat/dto/ChatMessageDto.java
+++ src/main/java/com/westsomsom/finalproject/chat/dto/ChatMessageDto.java
@@ -8,26 +8,35 @@ import lombok.*;
 @Getter
 @Setter
 //@AllArgsConstructor
-@NoArgsConstructor
+//@NoArgsConstructor
 public class ChatMessageDto {
     // 메시지  타입 : 입장, 채팅, 퇴장
     public enum MessageType{
         JOIN, TALK, LEAVE
     }
 
-    private MessageType messageType; // 메시지 타입
-    private int storeId; // 방번호
-    private String sender; // 메시지 보낸 사람
-    private String message; // 메시지
+    private MessageType messageType;
+    private int storeId;
+    private String sender;
+    private String message;
 
-    @JsonCreator
-    public ChatMessageDto(@JsonProperty("messageType") MessageType messageType,
-                          @JsonProperty("storeId") int storeId,
-                          @JsonProperty("sender") String sender,
-                          @JsonProperty("message") String message) {
+    // 기본 생성자 추가
+    public ChatMessageDto() {}
+
+    // setter 메서드 추가
+    public void setMessageType(MessageType messageType) {
         this.messageType = messageType;
+    }
+
+    public void setStoreId(int storeId) {
         this.storeId = storeId;
+    }
+
+    public void setSender(String sender) {
         this.sender = sender;
+    }
+
+    public void setMessage(String message) {
         this.message = message;
     }
 }
