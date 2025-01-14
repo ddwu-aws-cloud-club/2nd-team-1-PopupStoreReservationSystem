30e9e85db354c93431e7c113eee91eff642c9fab
seplease <seplease@naver.com>
2025-01-14 18:11:06 +0900

    Fix: ChatMessageDto 원래대로, ChatMessageSubscriber 로그 추가

--- src/main/java/com/westsomsom/finalproject/chat/dto/ChatMessageDto.java
+++ src/main/java/com/westsomsom/finalproject/chat/dto/ChatMessageDto.java
@@ -1,7 +1,5 @@
 package com.westsomsom.finalproject.chat.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Builder
