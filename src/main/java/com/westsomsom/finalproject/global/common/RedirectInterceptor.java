0fcaa8b41a43046e22b9ef5f0b95674426ba707f
karam Choi <riverchoi2523@gmail.com>
2025-01-17 02:40:24 +0900

    FIX:redirect filter

--- src/main/java/com/westsomsom/finalproject/global/common/RedirectInterceptor.java
+++ src/main/java/com/westsomsom/finalproject/global/common/RedirectInterceptor.java
@@ -11,7 +11,7 @@ public class RedirectInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
         if (response.getStatus() == HttpServletResponse.SC_MOVED_PERMANENTLY) {
-            response.setStatus(308);
+            response.setStatus(HttpServletResponse.SC_TEMPORARY_REDIRECT);
         }
     }
 }
